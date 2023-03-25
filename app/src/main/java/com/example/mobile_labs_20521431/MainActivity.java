1f3f771f8d7c4e3e7e50f263a0468033b0fa32c8
chaugiakhang193 <83385460+chaugiakhang193@users.noreply.github.com>
2023-03-25 20:11:52 +0700

    msg commit

--- app/src/main/java/com/example/mobile_labs_20521431/MainActivity.java
+++ app/src/main/java/com/example/mobile_labs_20521431/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.mobile_labs_20521431;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
